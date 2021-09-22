cac2602ac118bceabb62bce14297c50297af117f
s541910 <s541910@nwmissouri.edu>
2021-09-21 21:07:05 -0500

    Added CRUD apis

---
+++ ConnectCompanyBackend/ConnectCompanyBackend/src/main/java/com/nwmsu/ConnectCompanyBackend/ConnectCompanyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nwmsu.ConnectCompanyBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectCompanyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectCompanyBackendApplication.class, args);
+	}
+
+}
