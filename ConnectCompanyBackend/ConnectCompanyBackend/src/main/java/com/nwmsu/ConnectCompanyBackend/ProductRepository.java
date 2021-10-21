f1c6b38eb8e0e3b2e796627a587725934c2eb5c2
s541910 <s541910@nwmissouri.edu>
2021-10-21 12:01:45 -0500

    Removed Unwanted Files

--- ConnectCompanyBackend/ConnectCompanyBackend/src/main/java/com/nwmsu/ConnectCompanyBackend/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nwmsu.ConnectCompanyBackend;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-
-}
